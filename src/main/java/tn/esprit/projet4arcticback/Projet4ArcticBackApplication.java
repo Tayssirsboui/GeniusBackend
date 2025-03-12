0422a923ce74be7038d5df380881d07358e20b65
tayssirsboui <tayssirsboui@gmail.com>
2025-03-11 21:39:03 -0700

    Added initial project files

---
+++ src/main/java/tn/esprit/projet4arcticback/Projet4ArcticBackApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.projet4arcticback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet4ArcticBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet4ArcticBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/projet4arcticback/Projet4ArcticBackApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.projet4arcticback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet4ArcticBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
